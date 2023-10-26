cf74c3298c3c4a94bbb9c3849b02455b392b0473
darrius <mohamedtine17@gmail.com>
2023-10-26 12:53:37 +0000

    add functions for delete all tables in database

--- src/mysql/database.rs
+++ src/mysql/database.rs
@@ -23,8 +23,36 @@ pub async fn create_database(url: &str, name_db: String) -> Result<()> {
     Ok(())
 }
 
+// tables struct to get tables names
+#[derive(sqlx::FromRow)]
+struct Tables {
+    table_name: String
+}
+
+//function to get all tables from database name
+async fn show_table(connection: &Pool<MySql>, name_db: &String) -> Result<Vec<Tables>, String>{
+    let tables = sqlx::query_as::<_,Tables>(
+        "USE ?; \
+        SHOW TABLES;")
+        .bind(name_db)
+        .fetch_all(connection).await.expect("Error: Impossible to get tables");
+    Ok(tables)
+}
+
 //Query to delete all table in database
-pub async  fn delete_table(url: &str) -> Result<()>{
-    let conn =pool_connect(url).await.unwrap();
+pub async  fn delete_table(url: &str, name_db: String) -> Result<(), String>{
+    let conn = pool_connect(url).await.unwrap();
+    let db =  Database::new(name_db);
+    let tables = show_table(&conn, &db.name_db).await.unwrap_or_else(|err| {
+        panic!("{}", err);
+    });
+    for item in tables.iter() {
+        sqlx::query(
+            "USE ?;\
+            Drop Table ?")
+            .bind(&db.name_db)
+            .bind(&item.table_name)
+            .execute(&conn).await.expect("");
+    }
     Ok(())
 }
