744d9f856827c12aeb825617ef5019d78fdf7972
darrius <mohamedtine17@gmail.com>
2023-10-28 12:45:29 +0000

    delete function rn_db_mysql

--- src/mysql/database.rs
+++ src/mysql/database.rs
@@ -1,6 +1,7 @@
+use std::fmt::format;
 // modules for queries in database
-use sqlx::{MySqlPool, Result, Pool, MySql, Row};
-use sqlx::mysql::MySqlRow;
+use sqlx::{MySqlPool, Result, Pool, MySql};
+//use sqlx::mysql::MySqlRow;
 //use crate::types::{Database};
 
 //Connection Pool
@@ -13,7 +14,7 @@ async fn pool_connect_mysql(url: &str) -> Result<Pool<MySql>, String> {
 //Query to create database
 pub async fn create_db_mysql(url: &str, name_db: &String) -> Result<()> {
     let conn = pool_connect_mysql(url).await.unwrap();
-    let query = format!("CREATE DATABASE IF NOT EXISTS {}", name_db);
+    let query = format!("CREATE DATABASE IF NOT EXISTS {name_db}");
     sqlx::query(&query.trim())
         .execute(&conn).await
         .expect("Error: Impossible to create database");
@@ -22,17 +23,18 @@ pub async fn create_db_mysql(url: &str, name_db: &String) -> Result<()> {
 }
 
 // tables struct to get tables names
-#[derive(sqlx::FromRow)]
-struct Tables {
-    table_name: String
-}
+//#[derive(sqlx::FromRow)]
+//struct Tables {
+ //   table_name: String
+//}
 
 pub async fn delete_db_mysql(url: &str, name_db: &String) -> Result<(), String>{
     let conn = pool_connect_mysql(url).await
         .expect("Error: Connection failed");
-    sqlx::query("DROP DATABASE ?")
-        .bind(name_db)
-        .execute(&conn).await.expect("Error: Impossible to delete database");
+    let query = format!("DROP DATABASE {name_db}");
+    sqlx::query(&query)
+        .execute(&conn).await
+        .expect("Error: Impossible to delete database");
     conn.close().await;
     Ok(())
 }
@@ -43,18 +45,6 @@ pub async fn show_db_mysql(url: &str) -> Result<(), String>{
         .expect("Error: Connection failed");
     sqlx::query("SHOW DATABASE")
         .fetch(&conn);
-    conn.close().await;
-    Ok(())
-}
-
-// Rename database
-pub async fn rn_db_mysql(url: &str, old_name: &String, new_name: &String) -> Result<(), String> {
-    let conn = pool_connect_mysql(url).await
-        .expect("Error: Connection failed");
-    sqlx::query("RENAME DATABASE ? TO ?")
-        .bind(old_name)
-        .bind(new_name)
-        .execute(&conn).await.expect("Error: Impossible to rename database");
     conn.close().await;
     Ok(())
 }
--- tests/test_mysql.rs
+++ tests/test_mysql.rs
@@ -1,32 +1,30 @@
 #[cfg(test)]
 mod test_database{
     pub use server::mysql::database;
-    use sqlx::MySqlPool;
 
     const URL: &str= "mysql://root:@localhost:3306";
-    const URL_1: &str= "mysql://root:@localhost:3306/rust_test";
-
-    #[derive(sqlx::FromRow)]
-    struct Databases { name_db: String }
-    async fn find_database(name_db: String) -> Option<String> {
-        let pool = MySqlPool::connect(URL).await
-            .expect("Error: Connection is Impossible");
-        let db = sqlx::query_as::<_,Databases>("SHOW DATABASE")
-            .fetch_all(&pool).await.unwrap();
-        for i in db.iter(){
-            if i.name_db.clone() == name_db{
-                return Some(i.name_db.clone());
-            }
-        }
-        Some("Nothing".into())
-    }
+    //const URL_1: &str= "mysql://root:@localhost:3306/rust_test";
 
     #[tokio::test]
     async fn create_database(){
-        database::create_db_mysql(URL, &"test_21".to_string())
+        database::create_db_mysql(URL, &"test_2".to_string())
             .await
             .unwrap();
-        //let test = find_database("rust_test".to_string()).await.unwrap();
-        //assert_eq!("rust_test", test);
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn drop_database(){
+        database::delete_db_mysql(URL, &"test_2".to_string())
+            .await
+            .unwrap();
+    }
+
+    #[tokio::test]
+    async fn show_database(){
+        database::show_db_mysql(URL)
+            .await
+            .unwrap();
     }
 }
+
