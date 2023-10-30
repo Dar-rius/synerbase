00831f091e479a219a6e781e8f03ffc64ecd5c26
darrius <mohamedtine17@gmail.com>
2023-10-30 13:47:44 +0000

    fixed bug for function to delete all tables in db

--- src/mysql/database.rs
+++ src/mysql/database.rs
@@ -1,8 +1,7 @@
-use std::fmt::format;
 // modules for queries in database
 use sqlx::{MySqlPool, Result, Pool, MySql};
-//use sqlx::mysql::MySqlRow;
 //use crate::types::{Database};
+
 
 //Connection Pool
 async fn pool_connect_mysql(url: &str) -> Result<Pool<MySql>, String> {
@@ -22,11 +21,25 @@ pub async fn create_db_mysql(url: &str, name_db: &String) -> Result<()> {
     Ok(())
 }
 
-// tables struct to get tables names
-//#[derive(sqlx::FromRow)]
-//struct Tables {
- //   table_name: String
-//}
+
+async fn show_tb(conn: &Pool<MySql>) -> Result<Vec<String>, String> {
+    let tables = sqlx::query_scalar("SHOW TABLES")
+    .fetch_all(conn).await
+        .expect("Error: Impossible to found tables");
+    Ok(tables)
+}
+
+pub async fn delete_tables_mysql(url: &str) -> Result<(), String>{
+    let conn = pool_connect_mysql(url).await.unwrap();
+    let tables = show_tb(&conn).await.unwrap();
+    for item in tables {
+        let query = format!("DROP TABLE {item}");
+        sqlx::query(&query).execute(&conn).await
+            .expect("Error: Impossible to drop table");
+    }
+    Ok(())
+}
+
 
 pub async fn delete_db_mysql(url: &str, name_db: &String) -> Result<(), String>{
     let conn = pool_connect_mysql(url).await
--- tests/test_mysql.rs
+++ tests/test_mysql.rs
@@ -3,7 +3,7 @@ mod test_database{
     pub use server::mysql::database;
 
     const URL: &str= "mysql://root:@localhost:3306";
-    //const URL_1: &str= "mysql://root:@localhost:3306/rust_test";
+    const URL_1: &str= "mysql://root:@localhost:3306/rust_test";
 
     #[tokio::test]
     async fn create_database(){
@@ -25,6 +25,11 @@ mod test_database{
         database::show_db_mysql(URL)
             .await
             .unwrap();
+    }
+
+    #[tokio::test]
+    async fn database_empty(){
+        database::delete_tables_mysql(URL_1).await.unwrap()
     }
 }
 
