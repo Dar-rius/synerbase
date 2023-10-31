62d4daf4b2527c6a6aef8a057cfc047e83f38e51
darrius <mohamedtine17@gmail.com>
2023-10-31 00:01:34 +0000

    progress for rename rename database and add feature for backup database

--- src/mysql/database.rs
+++ src/mysql/database.rs
@@ -1,5 +1,7 @@
 // modules for queries in database
-use sqlx::{MySqlPool, Result, Pool, MySql};
+use sqlx::{MySqlPool, Result, Pool, MySql, query};
+use std::process::Command;
+use std::fs::write;
 //use crate::types::{Database};
 
 
@@ -10,8 +12,9 @@ async fn pool_connect_mysql(url: &str) -> Result<Pool<MySql>, String> {
         Ok(pool)
 }
 
+
 //Query to create database
-pub async fn create_db_mysql(url: &str, name_db: &String) -> Result<()> {
+pub async fn create_db_mysql(url: &str, name_db: &String) -> Result<(), String> {
     let conn = pool_connect_mysql(url).await.unwrap();
     let query = format!("CREATE DATABASE IF NOT EXISTS {name_db}");
     sqlx::query(&query.trim())
@@ -22,13 +25,17 @@ pub async fn create_db_mysql(url: &str, name_db: &String) -> Result<()> {
 }
 
 
+//Query to get all tables in databases
 async fn show_tb(conn: &Pool<MySql>) -> Result<Vec<String>, String> {
     let tables = sqlx::query_scalar("SHOW TABLES")
     .fetch_all(conn).await
         .expect("Error: Impossible to found tables");
+    conn.close().await;
     Ok(tables)
 }
 
+
+//Query to delete all tables
 pub async fn delete_tables_mysql(url: &str) -> Result<(), String>{
     let conn = pool_connect_mysql(url).await.unwrap();
     let tables = show_tb(&conn).await.unwrap();
@@ -37,13 +44,15 @@ pub async fn delete_tables_mysql(url: &str) -> Result<(), String>{
         sqlx::query(&query).execute(&conn).await
             .expect("Error: Impossible to drop table");
     }
+    conn.close().await;
     Ok(())
 }
 
 
+//Query to delete db
 pub async fn delete_db_mysql(url: &str, name_db: &String) -> Result<(), String>{
     let conn = pool_connect_mysql(url).await
-        .expect("Error: Connection failed");
+        .unwrap();
     let query = format!("DROP DATABASE {name_db}");
     sqlx::query(&query)
         .execute(&conn).await
@@ -52,13 +61,40 @@ pub async fn delete_db_mysql(url: &str, name_db: &String) -> Result<(), String>{
     Ok(())
 }
 
+
 //Query to show database
 pub async fn show_db_mysql(url: &str) -> Result<Vec<String>, String>{
     let conn = pool_connect_mysql(url).await
-        .expect("Error: Connection failed");
+        .unwrap();
     let databases = sqlx::query_scalar("SHOW DATABASES")
         .fetch_all(&conn).await
         .expect("Error: Impossible to get all databases");
     conn.close().await;
     Ok(databases)
+}
+
+
+pub fn backup_db_mysql(user: &String, name_db: &String, name_backup: &String) -> Result<(), String>{
+    let command = Command::new("cmd")
+        .args(&["mysqldump -u", user, "-p", name_db])
+        .output()
+        .expect("Error: Problem in dump Database");
+    if command.status.success(){
+        write(path, &command.stdout)
+            .expect("Error: Impossible to create backup");
+    }
+    Ok(())
+}
+
+
+//Query to rename database
+pub async fn rn_db_mysql(url_1: &str, url_2: &str, user: &String, old_name: &String, new_name: &String) -> Result<(), String>{
+    let query = format!("SOURCE {path}");
+    backup_db_mysql(user, old_name, new_name)?;
+    create_db_mysql(url_1, new_name).await?;
+    let conn  =  pool_connect_mysql(url_2).await?;
+    sqlx::query(&query).execute(&conn).await
+        .expect("Error: Impossible to use Database");
+    delete_db_mysql(url_1, old_name).await?;
+    Ok(())
 }
--- tests/test_mysql.rs
+++ tests/test_mysql.rs
@@ -2,8 +2,8 @@
 mod test_database{
     pub use server::mysql::database;
 
-    const URL: &str= "mysql://root:@localhost:3306";
-    const URL_1: &str= "mysql://root:@localhost:3306/rust_test";
+    const URL: &str= "mysql://root:@localhost:3307";
+    const URL_1: &str= "mysql://root:@localhost:3307/test_true";
 
     #[tokio::test]
     async fn create_database(){
@@ -22,7 +22,7 @@ mod test_database{
 
     #[tokio::test]
     async fn show_database(){
-        let test = database::show_db_mysql(URL)
+        database::show_db_mysql(URL)
             .await
             .unwrap();
     }
@@ -30,6 +30,22 @@ mod test_database{
     #[tokio::test]
     async fn database_empty(){
         database::delete_tables_mysql(URL_1).await.unwrap()
+    }
+
+    #[test]
+    fn backup_db(){
+        database::backup_db_mysql(&"root".into(),
+                              &"test_2".into(),
+                              &"back_test".into()).unwrap();
+    }
+
+    #[tokio::test]
+    async fn rename_db(){
+        database::rn_db_mysql(&URL,
+                              &URL_1,
+            &"root".into(),
+            &"new_test".into(), &"test_true".into()).await.unwrap();
+
     }
 }
 
